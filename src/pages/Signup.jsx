126fe43d3564a6a3d824b601df40677369a978a3
Bryan Herrera <bcherrera14@gmail.com>
2022-09-19 18:31:53 -0700

    add signin/up form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,11 +1,79 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
-export default class Signup extends Component {
-	render() {
-		return (
-			<div>
-				<h1>Signup</h1>
+function Signup() {
+	const [ showPassword, setShowPassword ] = useState(false);
+	const [ formData, setFormData ] = useState({ email: '', password: '', name: '' });
+	const { email, password, name } = formData;
+
+	const navigate = useNavigate();
+
+	const onChange = (e) => {
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.id]: e.target.value
+		}));
+	};
+
+	return (
+		<div>
+			<div className="pageContainer">
+				<header>
+					<p className="pageHeader">Welcome Back!</p>
+				</header>
+
+				<form>
+					<input
+						type="text"
+						className="nameInput"
+						placeholder="Name"
+						id="name"
+						value={name}
+						onChange={onChange}
+					/>
+					<input
+						type="email"
+						className="emailInput"
+						placeholder="Email"
+						id="email"
+						value={email}
+						onChange={onChange}
+					/>
+					<div className="passwordInputDiv">
+						<input
+							type={showPassword ? 'text' : 'password'}
+							className="passwordInput"
+							placeholder="Password"
+							id="password"
+							value={password}
+							onChange={onChange}
+						/>
+						<img
+							src={visibilityIcon}
+							alt="show password"
+							className="showPassword"
+							onClick={() => setShowPassword((prevState) => !prevState)}
+						/>
+					</div>
+					<Link to="/forgotpassword" className="forgotPasswordLink">
+						Forgot Password?
+					</Link>
+					<div className="signUpBar">
+						<p className="signUpText">Sign Up</p>
+						<button className="signUpButton">
+							<ArrowRightIcon fill="#ffff" width="34px" height="34px" />
+						</button>
+					</div>
+				</form>
+				<Link to="/sign-in" className="registerLink">
+					Sign In Instead?
+				</Link>
 			</div>
-		);
-	}
+		</div>
+	);
 }
+
+export default Signup;
