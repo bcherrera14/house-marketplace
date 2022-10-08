2cf1a3e49b452bf940fb64d1cddc590d6391d3c4
bcherrera14 <bcherrera14@gmail.com>
2022-10-07 18:13:58 -0700

    add toastify alerts

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase.config';
@@ -41,7 +42,7 @@ function Signup() {
 
 			navigate('/');
 		} catch (error) {
-			console.log(error);
+			toast.error('Something went wrong with registration.')
 		}
 	};
 
