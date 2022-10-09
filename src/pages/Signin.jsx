331b2d2f877f0d7eae350c7222a339f2f5b1e9b0
bcherrera14 <bcherrera14@gmail.com>
2022-10-08 20:32:59 -0700

    add google oauth

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+import OAuth from '../components/OAuth';
 
 function Signin() {
 	const [ showPassword, setShowPassword ] = useState(false);
@@ -79,6 +80,9 @@ function Signin() {
 						</button>
 					</div>
 				</form>
+
+				<OAuth/>
+
 				<Link to="/sign-up" className="registerLink">
 					Sign Up Instead?
 				</Link>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -7,6 +7,7 @@ import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+import OAuth from '../components/OAuth';
 
 function Signup() {
 	const [ showPassword, setShowPassword ] = useState(false);
@@ -96,6 +97,10 @@ function Signup() {
 						</button>
 					</div>
 				</form>
+				
+				<OAuth></OAuth>
+
+
 				<Link to="/sign-in" className="registerLink">
 					Sign In
 				</Link>
