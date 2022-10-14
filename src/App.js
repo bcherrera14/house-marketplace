d2a092877401515b1467a586b48743c484c26a73
bcherrera14 <bcherrera14@gmail.com>
2022-10-13 17:43:54 -0700

    add create listing page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import ForgotPassword from './pages/ForgotPassword';
 import Category from './pages/Category';
+import CreateListing from './pages/CreateListing';
 
 function App() {
 	return (
@@ -21,10 +22,12 @@ function App() {
 					<Route path="/category/:categoryName" element={<Category />} />
 					<Route path='/profile' element={<PrivateRoute/>}>
 						<Route path="/profile" element={<Profile />} />
+
 					</Route>
 					<Route path="/sign-in" element={<Signin />} />
 					<Route path="/sign-up" element={<Signup />} />
 					<Route path="/forgot-password" element={<ForgotPassword />} />
+					<Route path="/create-listing" element={<CreateListing />} />
 				</Routes>
 				<Navbar />
 			</Router>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,6 +5,8 @@ import {updateDoc, doc} from 'firebase/firestore'
 import {db} from '../firebase.config'
 import { useNavigate, Link } from 'react-router-dom';
 import {toast} from 'react-toastify'
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
 
 function Profile() {
 	const auth = getAuth()
@@ -70,6 +72,12 @@ function Profile() {
 					<input type="text" id="email" className={!changeDetails ? 'profileEmail' : 'profileEmailActive'} disabled={!changeDetails} value={email} onChange={onChange} />
 				</form>
 			</div>
+
+			<Link to='/create-listing' className='createListing'>
+				<img src={homeIcon} alt="home" />
+				<p>Sell or rent your home</p>
+				<img src={arrowRight} alt="arrow right" />
+			</Link>
 		</main>
 	</div>
 }
