3c8fd5528a198c59b349de0dfb30dce126cec005
bcherrera14 <bcherrera14@gmail.com>
2022-10-18 16:32:40 -0700

    create listing page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Signup from './pages/Signup';
 import ForgotPassword from './pages/ForgotPassword';
 import Category from './pages/Category';
 import CreateListing from './pages/CreateListing';
+import Listing from './pages/Listing';
 
 function App() {
 	return (
@@ -28,6 +29,7 @@ function App() {
 					<Route path="/sign-up" element={<Signup />} />
 					<Route path="/forgot-password" element={<ForgotPassword />} />
 					<Route path="/create-listing" element={<CreateListing />} />
+					<Route path="/category/:categoryName/:listingId" element={<Listing />} />
 				</Routes>
 				<Navbar />
 			</Router>
